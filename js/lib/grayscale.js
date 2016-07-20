501eb54edf15f9604aba0598cfdbb87b65f858bc
Casey Pavelko <cp320703@gmail.com>
2016-07-20 15:40:17 -0400

    major changes, bootstrap makes for good walls

--- js/lib/grayscale.js
+++ js/lib/grayscale.js
@@ -29,9 +29,9 @@ $(function() {
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
-    $('.navbar-toggle:visible').click();
-  }
+    if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+        $('.navbar-toggle:visible').click();
+    }
 });
 
 // Google Maps Scripts
@@ -57,7 +57,7 @@ function init() {
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
@@ -169,7 +169,7 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
